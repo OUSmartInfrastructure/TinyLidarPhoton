a03cd1e2f6ad30f717f674503e74ad78315de2dd
Ohio University's Smart Infrastructure <33726869+OUSmartInfrastructure@users.noreply.github.com>
2019-05-02 04:46:09 -0400

    Create tl.ino

---
+++ tl.ino
@@ -0,0 +1,72 @@
+#include <Wire.h>
+#define samples 1000 //number of samples for each loop
+unsigned long oldTime = 0;
+unsigned long timeNow = 0;
+unsigned long duration = 0;
+uint16_t i,j;
+
+void setMC(){
+      Wire.beginTransmission(0x10);
+      Wire.write('MC'); //send MC command for continuous mode
+      Wire.endTransmission();
+      delay(1000);
+
+      /*Wire.beginTransmission(0x10);  // take single measurement
+      Wire.write('Q');
+      Wire.endTransmission();
+
+      Wire.requestFrom(0x10, 2);    // request 2 bytes from tinyLiDAR
+      while(Wire.available()){   // slave may send less than requested
+          uint8_t c = Wire.read();    // receive a byte as character
+          Serial.println(c);         // print the character
+      }*/
+}
+
+void setup() {
+
+    Serial.begin(115200); //setup the serial port
+    Wire.setSpeed(100000); //set to 100KHz
+    Wire.begin();
+
+    setMC();
+} //setup
+
+void loop(){
+    Serial.println("[LOOP]");
+    for (j = 0; j < samples; j++){
+      Wire.beginTransmission(0x10);  // take single measurement
+      Wire.write('D');
+      Wire.endTransmission();
+
+  	  Wire.requestFrom(0x10, 2);    // request 2 bytes from tinyLiDAR
+
+      i = Wire.read();        // receive MSB byte
+      i = i<<8 | Wire.read(); // receive LSB byte and put them together
+
+      Serial.print(j);
+      Serial.print(":");
+      Serial.print(millis());
+      Serial.print(":");
+      Serial.println(i);
+      delay(30);
+    }
+    Serial.println("[FOR END]");
+
+    timeNow = millis(); //read the current time in milliseconds
+    Serial.print("[TIMENOW]");
+    Serial.print(timeNow);
+    Serial.print("<>[OLDTIME]");
+    Serial.println(oldTime);
+    duration = timeNow - oldTime;
+    Serial.print("<>[DURATION]");
+    Serial.println(duration);
+    oldTime = timeNow;
+    Serial.print(F("Effective Response Rate is ") );
+
+    float freq = samples/(duration*.001);
+    //float freq = 1/duration;
+    //Serial.print( samples/(duration*.001) );
+    Serial.print(freq);
+    Serial.println(F("Hz "));
+    //delay(10);
+}
